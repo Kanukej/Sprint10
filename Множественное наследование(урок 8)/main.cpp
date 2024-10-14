b31b2ed550924180daed6851fb96e8ba35f06c2c
Kanukej <160567252+Kanukej@users.noreply.github.com>
2024-10-14 15:25:58 +0600

    Create main.cpp

---
+++ Множественное наследование(урок 8)/main.cpp
@@ -0,0 +1,46 @@
+#include "svg.h"
+#include <cmath>
+
+template <typename DrawableIterator>
+void DrawPicture(DrawableIterator begin, DrawableIterator end, svg::ObjectContainer& target) {
+    for (auto it = begin; it != end; ++it) {
+        (*it)->Draw(target);
+    }
+}
+
+template <typename Container>
+void DrawPicture(const Container& container, svg::ObjectContainer& target) {
+    using namespace std;
+    DrawPicture(begin(container), end(container), target);
+}
+
+int main() {
+    using namespace svg;
+    using namespace shapes;
+    using namespace std;
+
+    vector<unique_ptr<svg::Drawable>> picture;
+    picture.emplace_back(make_unique<Triangle>(Point{100, 20}, Point{120, 50}, Point{80, 40}));
+    picture.emplace_back(make_unique<Star>(Point{50.0, 20.0}, 10.0, 4.0, 5));
+    picture.emplace_back(make_unique<Snowman>(Point{30, 20}, 10.0));
+
+    svg::Document doc;
+    DrawPicture(picture, doc);
+
+    const Text base_text =  //
+        Text()
+            .SetFontFamily("Verdana"s)
+            .SetFontSize(12)
+            .SetPosition({10, 100})
+            .SetData("Happy New Year!"s);
+    doc.Add(Text{base_text}
+                .SetStrokeColor("yellow"s)
+                .SetFillColor("yellow"s)
+                .SetStrokeLineJoin(StrokeLineJoin::ROUND)
+                .SetStrokeLineCap(StrokeLineCap::ROUND)
+                .SetStrokeWidth(3)
+                .SetFontWeight("bold"s));
+    doc.Add(Text{base_text}.SetFillColor("red"s).SetFontWeight("bold"s));
+    doc.Add(Text().SetData("Happy New Year!"s).SetStrokeWidth(double(1)/7).SetFontWeight("bold"s));
+    doc.Render(cout);
+}
